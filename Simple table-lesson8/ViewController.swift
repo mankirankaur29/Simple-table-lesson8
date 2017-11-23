4015ca110ee5c2f0ea2278f41f3855becebc8b89
Mankiran kaur <mankirankaur@Mankirans-MacBook-Pro.local>
2017-11-22 19:04:55 -0500

    Initial Commit

---
+++ Simple table-lesson8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple table-lesson8
+//
+//  Created by Mankiran kaur on 2017-11-22.
+//  Copyright © 2017 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
